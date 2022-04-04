ebbf2ea0f048bb5825d39b7cc40158d15c14c77e
xbg <xbg@foxmail.com>
2022-04-04 11:22:44 +0800

    CR

---
+++ miniprogram/projects/A00/skin/skin.js
@@ -0,0 +1,43 @@
+module.exports = {
+	PID: 'A00', //篮球场
+
+	NAV_COLOR: '#ffffff',
+	NAV_BG: '#131313',
+
+	MEET_NAME: '预约', 
+ 
+	MENU_ITEM: ['首页', '预约日历', '我的'], // 第1,4,5菜单
+
+	NEWS_CATE: '1=本馆动态,2=篮球知识',
+	MEET_TYPE: '1=场地预约|leftbig3',
+
+	DEFAULT_FORMS: [{
+			type: 'line',
+			title: '姓名',
+			desc: '请填写您的姓名',
+			must: true,
+			len: 50,
+			onlySet: {
+				mode: 'all',
+				cnt: -1
+			},
+			selectOptions: ['', ''],
+			mobileTruth: true,
+			checkBoxLimit: 2,
+		},
+		{
+			type: 'line',
+			title: '手机',
+			desc: '请填写您的手机号码',
+			must: true,
+			len: 50,
+			onlySet: {
+				mode: 'all',
+				cnt: -1
+			},
+			selectOptions: ['', ''],
+			mobileTruth: true,
+			checkBoxLimit: 2,
+		}
+	]
+}
